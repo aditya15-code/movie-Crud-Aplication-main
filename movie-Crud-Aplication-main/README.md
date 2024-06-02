f5d1c919d776f4a26ee3883439bc160ee9240117
Aditya Kumari <adityakumari24835@gmail.com>
2024-06-03 00:32:54 +0530

    first commit

---
+++ movie-Crud-Aplication-main/README.md
@@ -0,0 +1,28 @@
+Movies List Web Application
+This web application allows users to view, add, edit and delete movies from the list. The application is built using HTML, CSS and JavaScript.
+
+Functionalities
+Displaying a list of movies: The application displays a list of movies with a picture, title, year of release, category and rating. 
+Users can search the list of movies by entering a search term in the search box.
+Adding a new movie: Users can add a new movie by entering the image URL, title, year of release, 
+category and rating in the appropriate fields. After adding a movie, the list is refreshed.
+Editing an existing movie: Users can edit the data of an existing movie. By clicking on the "Edit" 
+button next to a specific movie, an editing form opens in which the title, year of release, category and rating of the movie can be changed.
+Deleting a movie: Users can delete a movie from the list by clicking the "Delete" button next to a specific movie.
+Technical information
+The application is built using the following technologies and libraries:
+
+HTML5
+CSS3 (used Bootstrap styling library)
+JavaScript (ES6)
+Instructions for launching the application
+To run this application locally, follow these steps:
+
+Download all files from this repository.
+Open the index.html file in your web browser.
+Note: This application uses the local version of the database, which is implemented in the db.js file.
+If you want to use a real database, you have to implement or integrate it with an existing backend system.
+
+Note
+This application is only a demonstration of basic functionality and is not intended for actual production use. 
+You can use it as a basis for developing similar web applications or as material for learning HTML, CSS and JavaScript technologies.
---
+++ movie-Crud-Aplication-main/main.js
@@ -0,0 +1,130 @@
+//SELECTORS
+const moviesView = document.querySelector('.moviesView');
+const addMoviesView = document.querySelector('.add-movies-view');
+const editMoviesView = document.querySelector('.edit-movies-view');
+
+
+//INPUTS
+const imageInput = document.querySelector('input[name="image"]');
+const titleInput = document.querySelector('input[name="title"]');
+const yearInput = document.querySelector('input[name="year"]');
+const categoryInput = document.querySelector('input[name="category"]');
+const rateInput = document.querySelector('input[name="rate"]');
+const searchInput = document.querySelector('#search');
+
+//EDIT INPUTS
+const eTitleInput = document.querySelector('input[name="etitle"]');
+const eYearInput = document.querySelector('input[name="eyear"]');
+const eCategoryInput = document.querySelector('input[name="ecategory"]');
+const eRateInput = document.querySelector('input[name="erate"]');
+
+
+//Buttons
+const addViewBtn = document.querySelector('#addViewBtn');
+const addButton = document.querySelector('#addBtn');
+const editButton = document.querySelector('#editBtn');
+
+
+//Events
+addViewBtn.addEventListener('click', () => {
+    moviesView.style.display = "none";
+    addMoviesView.style.display = "block";
+})
+addButton.addEventListener('click', addMovie);
+editButton.addEventListener('click', editMovie);
+searchInput.addEventListener('keyup', searchMovie);
+
+
+//Functions
+function searchMovie() {
+    let searchTerm = this.value;
+    let filtered = db.filter(movie => movie.title.includes(searchTerm));
+    displayList(filtered);
+}
+function addMovie() {
+    let newMovie = {
+        image: imageInput.value,
+        title: titleInput.value,
+        year: yearInput.value,
+        category: categoryInput.value,
+        rate: rateInput.value
+    }
+    db.push(newMovie)
+    displayList();
+    moviesView.style.display = "flex";
+
+}
+function editMovie() {
+ let indexOfMovie = this.getAttribute('data-index');
+ let editedMovie = {
+     image: db[indexOfMovie].image,
+     title: eTitleInput.value,
+     year: eYearInput.value,
+     category: eCategoryInput.value,
+     rate: eRateInput.value
+    }
+
+    db[indexOfMovie] = editedMovie;
+    editMoviesView.style.display='none';
+    moviesView.style.display = 'flex';
+    displayList();
+
+}
+
+function deleteMovie() {
+    let indexOfMoive = this.getAttribute('data-index');
+    db.splice(indexOfMoive, 1);
+    displayList();
+}
+
+
+
+displayList();
+function displayEditForm() {
+    let indexOfMovie = this.getAttribute('data-index');
+    let selectedMovie = db[indexOfMovie]
+
+    eTitleInput.value = selectedMovie.title;
+    eCategoryInput.value = selectedMovie.category;
+    eYearInput.value = selectedMovie.year;
+    eRateInput.value = selectedMovie.rate;
+
+    editButton.setAttribute('data-index', indexOfMovie);
+    moviesView.style.display="none"
+    editMoviesView.style.display="block"
+}
+function displayList(filtered) {
+    let currentMovies = filtered || db;
+    let html = ``;
+    currentMovies.forEach((movie,index) => {
+        html += `
+            <div class="card col-sm-2" style="width: 18rem;">
+                <img class="card-img-top" src="${movie.image}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${movie.title}</h5>
+                    <p class="card-text">${movie.year}</p>
+                    <p class="card-text">${movie.category}</p>
+                    <p class="card-text">${movie.rate}</p>
+                    <a class="btn btn-warning editBtn" data-index="${index}">Edit</a>
+                    <a class="btn btn-danger deleteBtn" data-index="${index}">Delete</a>
+                </div>
+           </div>   
+        `.trim()
+
+
+    })
+
+    moviesView.innerHTML = html;
+
+    const editBtn = document.querySelectorAll('.editBtn');
+    const deleteBtn = document.querySelectorAll('.deleteBtn');
+
+    editBtn.forEach((btn,index) => {
+        btn.addEventListener('click', displayEditForm);
+        deleteBtn[index].addEventListener('click', deleteMovie);
+    })
+    addMoviesView.style.display = "none";
+}
+
+
+
